9651637ba46a16c5131acdec9df4ebef7114a907
Matthew Gleich <matthewgleich@gmail.com>
2019-12-23 22:43:43 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  
+  runApp(MyApp()); // Runs our app
 }
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Text("Hello!"), // Home defines the main widget that will apear on the screen
+    );
   }
 }
