215d7e247c10360014d57d881c34a615a6ef7c9d
Matthew Gleich <matthewgleich@gmail.com>
2019-12-31 15:47:50 -0500

    Create quiz.dart

---
+++ lib/coreWidgets/quiz.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import './../widgets/question.dart';
+import './../widgets/answer.dart';
+
+class Quiz extends StatelessWidget {
+  final List<Map<String, Object>> questions;
+  final int questionIndex;
+  final Function answeredQuestion;
+  final Function restart;
+  Quiz(
+      {@required this.questions,
+      @required this.answeredQuestion,
+      @required this.questionIndex,
+      @required this.restart});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Question(questions[questionIndex]["question"]),
+        ...(questions[questionIndex]["answers"] as List<String>).map(
+          (answer) {
+            return Answer(answeredQuestion, answer);
+          },
+        ).toList(),
+        Container(
+          width: double.infinity,
+          margin: EdgeInsets.all(60),
+          child: RaisedButton(
+            child: Text("Restart"),
+            color: Colors.red,
+            textColor: Colors.white,
+            onPressed: restart,
+          ),
+        ),
+      ],
+    );
+  }
+}
